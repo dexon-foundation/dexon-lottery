52a4aed93f42d82b19e1d73caf95bf9dbfabe441
Hsuan Lee <boczeratul@gmail.com>
2019-04-16 18:09:34 +0800

    Update contract address

--- truffle.js
+++ truffle.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -12,9 +12,10 @@
  *   },
  */
 
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var secret = require('./secret');
-var mnemonic = secret.mnemonic;
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const secret = require('./secret');
+
+const mnemonic = secret.mnemonic;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -22,21 +23,22 @@ module.exports = {
   networks: {
     testnet: {
       provider: new HDWalletProvider(mnemonic,
-        "https://testnet-rpc.dexon.org", 0, 1, true, "m/44'/237'/0'/0/"),
-      network_id: "*",
+        'https://testnet-rpc.dexon.org', 0, 1, true, "m/44'/237'/0'/0/"),
+      network_id: '*',
       gasPrice: 24000000000,
     },
 
     development: {
-      network_id: "*",
-      host: "localhost",
+      network_id: '*',
+      host: 'localhost',
       port: 8545,
       gasLimit: 7984452,
     },
   },
+
   // compilers: {
   //   solc: {
-  //     version: "0.5.2" // ex:  "0.4.20". (Default: Truffle's installed solc)
-  //   }
-  // }
+  //     version: '0.5.2', // ex:  "0.4.20". (Default: Truffle's installed solc)
+  //   },
+  // },
 };
